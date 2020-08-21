672b927bcf6cb37dfdd9974afdf037854912d61e
Alex811 <811alex@gmail.com>
2020-08-21 12:54:10 +0300

    Rollback cleanup call when killing a session the user might not have permissions on the parrent directory

--- serverboottool.sh
+++ serverboottool.sh
@@ -313,7 +313,6 @@ function kill {
   if [ -e "$socketdir/$1" ]; then
     tmux -S "$socketdir/$1" kill-session &&
     echo "Sent kill signal: $1"
-    cleanup
   else
     echo "Socket not found: $1"
     exit 6
